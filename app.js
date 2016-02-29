cb198a5c01584e8167ab058a89dd01faeccf9574
Matthew Keymer <mastercyrax@hotmail.com>
2016-02-29 10:52:49 -0600

    Updated input info

--- app.js
+++ app.js
@@ -17,11 +17,13 @@ $(document).ready(function(){
 
       // This clears out the form.
       $('#salaryForm').find('input[type=text]').val('');
+      $('#salaryForm').find('input[type=number]').val('');
 
       // This pushes the object into an array.
       employeeArray.push(values);
 
       salaryCalc();
+      displayInfo(values);
     };
   });
 
@@ -31,37 +33,35 @@ $(document).ready(function(){
 
 var employeeArray = [];
 
-// This calculates the salaries.
+// This calculates the amount paid monthly in salaries.
 function salaryCalc(){
   var finalSalary = 0
   for (var i = 0; i < employeeArray.length; i++){
-    var employees = employeeArray[i];
-    finalSalary += parseInt(employees.salary) / 12;
+    var employee = employeeArray[i];
+    finalSalary += parseInt(employee.salary) / 12;
   };
 
-  displayInfo(employees);
-
-  // This displays the employee information that was entered.
-  function displayInfo(employee){
-    $(".output-area").append('<div class="employee"></div>');
-    var $el = $(".output-area").children().last();
-
-    $el.append("<li>" + employee.firstname + " " + employee.lastname + "</li>");
-    $el.append("<li>Employee ID: " + employee.employeeid + "</li>");
-    $el.append("<li>Job Title: " + employee.jobtitle + "</li>");
-    $el.append("<li>Yearly Salary: $" + employee.salary + "</li>");
-    $el.append("<button class='remove'>Remove Entry</button>");
-
-  };
-
-  // This allows the user to delete an entry.
-  function deleteInfo(){
-    $(this).parent().remove();
-
-  };
-
-  $('.total-monthly').text('Money Spent Monthly on Employee Salaries: ' + Math.round(finalSalary));
+  $('.total-monthly').text('Money Spent Monthly on Employee Salaries: $' + Math.round(finalSalary));
 
   $('.output-area').on('click', '.remove', deleteInfo);
+
+};
+
+// This displays the employee information that was entered.
+function displayInfo(employee){
+  $(".output-area").append('<div class="employeePane"></div>');
+  var $el = $(".output-area").children().last();
+
+  $el.append("<li>" + employee.firstname + " " + employee.lastname + "</li>");
+  $el.append("<li>Employee ID: " + employee.employeeid + "</li>");
+  $el.append("<li>Job Title: " + employee.jobtitle + "</li>");
+  $el.append("<li>Yearly Salary: $" + employee.salary + "</li>");
+  $el.append("<button class='remove'>Remove Entry</button>");
+
+};
+
+// This deletes an entry when the user clicks the "Remove Entry" button.
+function deleteInfo(){
+  $(this).parent().remove();
 
 };
